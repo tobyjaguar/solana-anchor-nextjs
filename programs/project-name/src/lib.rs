b90412c84393ff6aaf590fb3a230f8f6fdaf97fb
tobyjaguar <talgya@gmail.com>
2025-05-02 21:49:29 -0700

    update project name to a generic

--- programs/project-name/src/lib.rs
+++ programs/project-name/src/lib.rs
@@ -1,9 +1,9 @@
 use anchor_lang::prelude::*;
 
-declare_id!("9kb3RJ2M6fqfUR76V8tZbJX3WDQKuTZAspthw23LEbqW");
+declare_id!("");
 
 #[program]
-pub mod voting_app_2 {
+pub mod project_name {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
